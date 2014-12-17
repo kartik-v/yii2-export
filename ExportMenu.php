539f8985fb4c84ff4d4538b1a1b27df194a388d7
maxlapko <maxlapko@gmail.com>
2014-12-17 10:30:18 +0300

    Allow skip not needed export types.
    Allow skip not needed export types.

--- ExportMenu.php
+++ ExportMenu.php
@@ -411,6 +411,9 @@ class ExportMenu extends GridView
     {
         $items = $this->asDropdown ? [] : '';
         foreach ($this->exportConfig as $format => $settings) {
+            if (empty($settings)) {
+                continue;
+            }
             $icon = '';
             $label = '';
             if (!empty($settings['icon'])) {
