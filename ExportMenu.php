726811a1401fabba5d2ba0ed887767335ec43111
LosC soL <loscsol@gmail.com>
2018-01-17 15:26:19 +0100

    Fix: Return if raiseEvent() not True

--- ExportMenu.php
+++ ExportMenu.php
@@ -766,7 +766,7 @@ class ExportMenu extends GridView
         }
         $file = self::slash($this->folder) . $this->filename . '.' . $config['extension'];
         $cleanup = function () use ($file, $config) {
-            if ($this->raiseEvent('onGenerateFile', [$config['extension'], $this]) !== false) {
+            if ($this->raiseEvent('onGenerateFile', [$config['extension'], $this]) !== true) {
                 return;
             }
             if ($this->deleteAfterSave) {
